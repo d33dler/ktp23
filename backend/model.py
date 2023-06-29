6502f984b9bf109aa52672a35900a738b7f5a6c9
d33dler <r.rebeja@student.rug.nl>
2023-06-29 21:50:16 +0200

    added OLS regression and update RF to xgboost model

--- backend/model.py
+++ backend/model.py
@@ -2,11 +2,15 @@ import binascii
 import json
 import logging
 import os
+import pickle
 from copy import deepcopy
-from typing import List
+from dataclasses import dataclass
+from typing import List, Dict
 
 import numpy as np
 from anytree import NodeMixin, RenderTree
+from pandas._libs.parsers import CategoricalDtype
+
 from form_fields import Att
 from anytree.exporter import DotExporter
 import pandas as pd
@@ -16,6 +20,11 @@ from sklearn.model_selection import train_test_split
 import xgboost as xgb
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from hyperopt.pyll import scope
+from scipy.optimize import curve_fit
+from anytree import Node, RenderTree, findall, Walker
+import statsmodels.formula.api as smf
+
+pd.options.mode.chained_assignment = None  # default='warn'
 
 
 class PEMNode(NodeMixin):  # NodeMixin makes this class become a tree node
@@ -30,6 +39,8 @@ class PEMNode(NodeMixin):  # NodeMixin makes this class become a tree node
         self.value = value
         self.prev_choice = None
         self.prev_choice_ix = -1
+        self.ols_coef: dict = None
+        self.ols_model = None
         if children:  # set children only if given
             self.children = children
 
@@ -45,12 +56,13 @@ class PropertyEvaluationModel:
     sqm = 1
     node_set = {}
     nodes = {}
-    dataset_apartments = None
+    tree = None
+    dataset = None
     auxiliary_reg = None
 
     def __init__(self, apartments_ds_path, nodes_data_path):
-        self.nodes_data = json.load(open(nodes_data_path, "r"))
-        self.dataset_apartments = pd.read_csv(apartments_ds_path)
+        self.model_cfg = json.load(open(nodes_data_path, "r"))
+        self.dataset = pd.read_csv(apartments_ds_path)
         # create info logger object from logging module
         self.logger = logging.getLogger("PEM")
         # set logging level
@@ -65,10 +77,10 @@ class PropertyEvaluationModel:
         # add handler to logger
         self.logger.addHandler(handler)
 
-        self._fit_auxiliary_regressor(self.dataset_apartments)
-        self._build_tree(self.nodes_data)
+        self._build_tree(self.model_cfg)
+        self._fit_auxiliary_regressor(self.dataset)
 
-    def _fit_auxiliary_regressor(self, dataset):
+    def _fit_auxiliary_regressor(self, dataset: pd.DataFrame):
         # load stored model if exists
         if os.path.exists("auxiliary_regressor.json"):
             self.logger.info("Loading auxiliary regressor from file...")
@@ -76,16 +88,21 @@ class PropertyEvaluationModel:
             self.auxiliary_reg.load_model("auxiliary_regressor.json")
             return
         features = {'y': float}
-        for node in self.nodes_data["nodes"]:
+        for node in self.model_cfg["nodes"]:
             features.update({ft['f']: ft['dtype'] for ft in node["FID"]})
 
-        self.logger.info(f"Auxiliary regressor features: {features} ")
-        dataset["y"] = dataset["y"] / dataset["area_m2"]
+        self.logger.info(f"Auxiliary regressor features: \n {features} ")
 
-        dataset = dataset[features.keys()]
+        dataset = dataset[
+            list(set(self.model_cfg["regressors"]).union(set(features.keys())).intersection(set(dataset.columns)))]
+        dataset = dataset.dropna()
+
         # set all columns to categorical
         for col, dtype in features.items():
-            dataset[col] = dataset[col].astype(dtype)
+            try:
+                dataset[col] = dataset[col].astype(dtype)
+            except pd.errors.IntCastingNaNError:
+                self.logger.warning(f"Could not convert column {col} to {dtype}")
         # set y to float
         dataset["y"] = dataset["y"].astype(float)
         # separate X and y
@@ -95,21 +112,18 @@ class PropertyEvaluationModel:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=42)
         # xgboost random forest parameters
         params = {
-            'tree_method': 'gpu_hist',
+            'tree_method': 'hist',
             'objective': 'reg:squarederror',
             'nthread': 4,
-            'seed': 27,
+            'seed': 33,
             'max_cat_to_onehot': 6,
             'eta': 0.7,
-            'max_depth': 6,
-            'min_child_weight': 12,
+            'max_depth': 8,
+            'min_child_weight': 5,
             'subsample': 1,
-            'colsample_bytree': 0.6,
-            'gamma': 19,
-            'lambda': 0.6,
-            'num_parallel_tree': 167,
-
-
+            'colsample_bytree': 0.8,
+            'gamma': 5,
+            'lambda': 0.5,
         }
         # search_space = {
         #     'eta': hp.quniform('eta', 0.2, 1, 0.1),
@@ -154,6 +168,7 @@ class PropertyEvaluationModel:
         dmatrix_test = xgb.DMatrix(X_test, label=y_test, enable_categorical=True)
         self.logger.info(f"Mean absolute error: {np.mean(np.abs(model.predict(dmatrix_test) - y_test))} ")
         self.auxiliary_reg = model
+        return model
 
     def _build_tree(self, data):
         node_set, nodes = self.node_set, self.nodes
@@ -162,6 +177,7 @@ class PropertyEvaluationModel:
             node_set[node_data["ID"]] = node_data
 
         node_count = dict()
+        self.dataset["y"] = self.dataset["y"] / self.dataset["area_m2"]
 
         def recursive_tree_gen(parent, _node: str, child_ix: int, prev_choice=None):
             """
@@ -213,7 +229,9 @@ class PropertyEvaluationModel:
 
         recursive_tree_gen(None, "0", 0)
         leaf_nodes_len = len([n for n in nodes.values() if len(n.children) == 0])
-        leaves_ls = collect_leaves(nodes["0"], [])
+        root = nodes["0"]
+        self.tree = root
+        leaves_ls = collect_leaves(root, [])
 
         def recursive_sub_interval_creation(_data, _node, idx):
             if _data["_node_"] is not None:
@@ -229,14 +247,84 @@ class PropertyEvaluationModel:
                     lf.value = val_distribution[lf_ix]
                 return idx
 
-        recursive_sub_interval_creation(data["value_distribution"], nodes["0"], 0)
-        self.logger.info("Tree built successfully!")
-        self.logger.info("Tree structure:")
+        recursive_sub_interval_creation(data["value_distribution"], root, 0)
+
+        # Traverse the tree to find all leaf nodes
+        formula = "y ~ " + " + ".join(self.model_cfg["regressors"])
+
+        def fit_model(df, _formula):
+            return smf.ols(_formula, data=df).fit()
+
+        def generate_paths(node_id, path):
+            paths = []
+
+            # Get node from model_cfg
+            node = next((n for n in self.model_cfg["nodes"] if n["ID"] == str(node_id)), None)
+
+            if node is None:
+                raise ValueError(f"Node with ID {node_id} not found")
+
+            # If node has options, then it is not a leaf node
+            if node["OPTIONS"]:
+                for idx, option in enumerate(node["OPTIONS"]):
+                    new_path = path.copy()
+                    new_path.append(idx)
+                    paths += generate_paths(str(option[1]), new_path)
+            else:
+                paths.append(path)
+
+            return paths
+
+        @dataclass
+        class Scenario:
+            node_value: float
+            feature_values: pd.Series()
+            leaf: PEMNode
+
+        def filter_data_for_scenario(_scenario: Scenario, df):
+            filtered_df = df.copy()
+            for column in _scenario.feature_values.columns:
+                val = (_scenario.feature_values[column].astype(str) if _scenario.feature_values[
+                                                                           column].dtype.name == 'category' else
+                       _scenario.feature_values[column])[0]
+                filtered_df = filtered_df.loc[filtered_df[column] == val, :]
+            return filtered_df
+
+        # 1. Generate all path scenarios
+        # load OLS models if available
+        if os.path.exists(self.model_cfg["ols_model_path"]):
+            with open(self.model_cfg["ols_model_path"], 'rb') as f:
+                curve_params = pickle.load(f)
+        else:
+            star_scenarios = generate_paths(0, [])  # will be same length as leaf_nodes_len
+            star_scenarios = [Scenario(*argz) for argz in
+                              [self._traverse(root, unique_path, pd.DataFrame()) for unique_path in star_scenarios]]
+            # 2. Curve fitting for each scenario
+            curve_params = {}
+            for ix, scenario in enumerate(star_scenarios):
+                filtered_data = filter_data_for_scenario(scenario, self.dataset)
+                if not filtered_data.empty or len(filtered_data) >= self.model_cfg["reg_min_samples"]:
+                    curve_params[scenario.leaf.name] = fit_model(filtered_data, formula)
+                else:
+                    print(f"No data available for leaf scenario {ix}, skipping.")
+        self.logger.info("========== Model tree & OLS models built successfully! ==========")
+        # curve_params now holds the linear regression coefficients for each possible scenario
+        self.logger.info(f"Curve fitting completed successfully! Available sub-models: {len(curve_params)}")
+        self.logger.info(f"Stray leaves: {leaf_nodes_len - len(curve_params)}")
+        self.logger.info(f"Wrapping up...")
+        # save curve_params as pickle
+        with open(self.model_cfg["ols_model_path"], "wb") as f:
+            pickle.dump(curve_params, f, protocol=pickle.HIGHEST_PROTOCOL)
+        # save model params (OLS coefficients into leaf nodes)
+        for leaf in leaves_ls:
+            leaf.ols_coef = curve_params.get(leaf.name, None)
+            leaf.ols_coef = leaf.ols_coef.params.to_dict() if leaf.ols_coef is not None else None
+        # self.logger.info("Tree structure:")
         # for pre, fill, node in RenderTree(nodes["0"]):  # Assuming that the root node has the ID 0
         #     print(
         #         f"{pre}>({node.prev_choice}) = {node.value:0.2f} €") if len(
         #         node.children) == 0 else print(f"{pre}>({node.prev_choice})  {node.question} ")
-        self.logger.info("Tree statistics:")
+        self.logger.info("======================= Model statistics ========================")
         self.logger.info("Total nodes: %d" % len(nodes))
         self.logger.info("Total leaf nodes: %d " % leaf_nodes_len)
         DotExporter(nodes["0"], graph='graph', indent=1,
@@ -244,9 +332,26 @@ class PropertyEvaluationModel:
                     nodeattrfunc=lambda _: "shape=box",
                     edgeattrfunc=lambda _, child: 'label="%s"' % child.prev_choice or "",
                     edgetypefunc=lambda _, __: "--").to_dotfile("tree.dot")
-        JsonExporter(indent=2, sort_keys=True).write(nodes["0"], open("full_model.json", "w"))
+        JsonExporter(indent=2, sort_keys=True).write(root, open("full_model.json", "w"))
+        self.logger.info("Finished writing tree to file.")
+        self.logger.info("================================================================")
+        for leaf in leaves_ls:
+            leaf.ols_model = curve_params.get(leaf.name, None)
 
-    def _predict_reg_aux(self, features: list):
+    def _traverse(self, node, choices, reg_ft):
+        if len(node.children) == 0:
+            return node.value, reg_ft, node
+        if len(choices) > 0:
+            choice_ix = choices.pop(0)  # Pop the first answer
+            for child in node.children:
+                if child.prev_choice_ix == choice_ix:  # If the child's value matches the answer
+                    # add potential FID to the regression features
+                    if node.fid:
+                        for ft in node.fid:
+                            reg_ft[ft['f']] = pd.Series([ft['o'][choice_ix]], dtype=ft['dtype'])
+                    return self._traverse(child, choices, reg_ft)
+
+    def _predict_reg_aux(self, features: dict):
         """
         :param features: dictionary of features
         :type features: dict
@@ -254,45 +359,44 @@ class PropertyEvaluationModel:
         :rtype: float
         """
 
-        def traverse(node, choices, reg_ft):
-            if len(node.children) == 0:
-                return node.value, reg_ft
-            if len(choices) > 0:
-                choice_ix = choices.pop(0)  # Pop the first answer
-                for child in node.children:
-                    if child.prev_choice_ix == choice_ix:  # If the child's value matches the answer
-                        # add potential FID to the regression features
-                        if node.fid:
-                            for ft in node.fid:
-                                reg_ft[ft['f']] = pd.Series([ft['o'][choice_ix]], dtype=ft['dtype'])
-                                print(ft['f'])
-                        return traverse(child, choices, reg_ft)
-
         # traverse the tree and collect the regression features
-        valuation, reg_features = traverse(self.nodes["0"], features, pd.DataFrame())
+        valuation, reg_features, leaf = self._traverse(self.tree, features["idx"], pd.DataFrame())
         # create a dataframe from the regression features
         # create dmatrix
+        extra = pd.DataFrame({k: [v] for k, v in features["extra"].items()})
+        # drop columns in reg_features that appear in extra
+        reg_features = reg_features.drop(columns=extra.columns, errors="ignore")
+        # combine the two dataframes
+        reg_features = pd.concat([reg_features, extra], axis=1, join="inner")
+        reg_features = reg_features.reindex(columns=self.auxiliary_reg.feature_names)
         reg_features = xgb.DMatrix(reg_features, enable_categorical=True)
         # predict the valuation
         pred = self.auxiliary_reg.predict(reg_features)[0]
         # change code below to logging module
-
+        ols_pred =  leaf.ols_model.predict(extra)[0] if leaf.ols_model else None
         self.logger.info("--------------------------")
-        self.logger.info("Main system valuation: %f" % valuation)
-        self.logger.info("RF-Predicted valuation: %f" % pred)
+        self.logger.info("Main system linear valuation (expert supplied): %f" % valuation)
+        self.logger.info("Main system regression estimation : %f" % ols_pred)
+        self.logger.info("XGB-Predicted valuation: %f" % pred)
         self.logger.info("Abs. difference: %f" % abs(valuation - pred))
         self.logger.info("Mean: %f " % np.mean([valuation, pred]))
         self.logger.info("--------------------------")
-        return np.mean([valuation, pred])
+        return np.mean([valuation, pred]) if ols_pred is None else np.mean([valuation, pred, ols_pred])
 
-    def forward(self, _input: List[int]):
+    def forward(self, _input: dict):
         # collect nodes FIDs for regression
         return self._predict_reg_aux(_input)
 
 
 if __name__ == "__main__":
     pem = PropertyEvaluationModel("../resources/999MD_apartments_processed.csv", "model.json")
-
     # dummy input
-    _input = [0, 1, 0, 1, 0, 1, 1, 0, 1, 1]
+    _input = {'idx': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              'extra': {
+                  'dist_to_center': 2,
+                  'num_floors': 5,
+                  'num_rooms': 2,
+                  'area_m2': 50,
+                  'bathroom': 1,
+              }}
     pem.forward(_input)
